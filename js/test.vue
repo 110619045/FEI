5bbe6f7a1798c26334141e36fd30957c6215ab8a
110619045 <0304phoebe@gmail.com>
2024-07-18 04:23:09 +0800

    Initial commit

---
+++ js/test.vue
@@ -0,0 +1,16 @@
+<!-- <template>
+    <div>
+      <input type="checkbox" id="todo-item" checked="false" />
+      <label for="todo-item">My Todo Item</label>
+    </div>
+</template>
+<script>
+  export default {};
+</script> -->
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
