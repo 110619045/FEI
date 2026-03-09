d130deb70f2ff44b2ab20586e4a39e86509d4f40
110619045 <0304phoebe@gmail.com>
2026-03-09 20:44:39 +0800

    CHILLPAL ALL

--- js/core.js
+++ js/core.js
@@ -1,14 +1,5 @@
 const WORKS_DATA = [
     {
-        img: 'image/UnlockingInnerVision/ganzin.png',
-        link: 'work/UnlockingInnerVision.html',
-        Ename: 'Unlocking Inner Vision',
-        badge: 'DEVICE & SYSTEM',
-        tool: [{ tools: 'Python' }],
-        Esynopsis: 'Development of an assistive system...',
-        imgPosition: 'center center'
-    },
-    {
         img: 'image/Illusory Eyescape/0.jpg',
         link: 'work/IllusoryEyescape.html',
         Ename: 'Illusory Eyescape',
@@ -16,6 +7,15 @@ const WORKS_DATA = [
         tool: [{ tools: 'HTML' }, { tools: 'JS' }],
         Esynopsis: 'In recent years, the rapid development of generative AI...',
         imgPosition: 'center 33%'
+    },
+    {
+        img: 'image/UnlockingInnerVision/ganzin.png',
+        link: 'work/UnlockingInnerVision.html',
+        Ename: 'Unlocking Inner Vision',
+        badge: 'DEVICE & SYSTEM',
+        tool: [{ tools: 'Python' }],
+        Esynopsis: 'Development of an assistive system...',
+        imgPosition: 'center center'
     },
     {
         img: './image/友善地圖/0.png',
