8b83723069901ff58dc36a512e25351d79a6c162
110619045 <0304phoebe@gmail.com>
2026-03-23 07:11:12 +0800

    涼拌小秘書

---
+++ js/archive.js
@@ -0,0 +1,70 @@
+const WORKS_DATA = [
+    {
+        img: 'image/UnlockingInnerVision/ganzin.png',
+        link: 'work/UnlockingInnerVision.html',
+        Ename: 'Unlocking Inner Vision',
+        badge: 'DEVICE & SYSTEM',
+        tool: [{ tools: 'Python' }],
+        Esynopsis: 'Development of an assistive system...',
+        imgPosition: 'center center'
+    },
+    {
+        img: 'image/涼拌小秘書/G1.webp',
+        name: '涼拌小秘書',
+        link: 'work/ChillPal.html',
+        Ename: 'Chill Pal Secretary',
+        badge: 'APP & MINI APP',
+        tool: [
+            { tools: 'Python' },
+            { tools: 'JS' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '獨居或是平日無法與家人相處的長輩們，時常會有孤獨的心理狀態，這會間接的影響他們的身體狀況。我們藉由一系列使用者研究探索台灣長者的社交習慣，提出了讓老者能輕鬆的在Line上與人工智慧聊天並創作屬於自己的長輩圖，同時也協助他們解決各方面的問題。',
+        Eintro: "Elderly people who live alone or are unable to spend time with their families during the week often experience feelings of loneliness, which can indirectly affect their physical health. Through a series of user studies exploring the social habits of the elderly in Taiwan, we proposed a solution that allows seniors to easily chat with artificial intelligence on Line and create their own personalized elder memes. This also helps them solve various problems they may encounter.",
+        imgPosition: ''
+    },
+    {
+        img: './image/Pick Up Memories/0.jpg',
+        name: '打包回憶錄',
+        Ename: 'Pick Up Memories',
+        badge: 'GAME',
+        tool: [
+            { tools: 'UNREAL' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '現今的社會型態使得越來越多人必須搬到非成長的都市打拼生活。離開家鄉長年孤單一人在外地，總會想帶一些充滿回憶的物品在新的住處陪伴自己。因此藉由選擇回憶中的物品作為一個契機，發想出一款近似人格測試的遊戲，檢視目前玩家所嚮往的未來與了解玩家努力生活的動力來源。',
+        Eintro: "In today's society, more and more people are moving to cities different from where they grew up in order to pursue their careers. Living alone in a new place away from their hometown for years often makes them want to bring along items full of memories to keep them company. Inspired by this, we developed a game similar to a personality test that encourages players to choose items from their memories. This serves as an opportunity to examine the future they aspire to and understand the sources of motivation driving their efforts in life.",
+    },
+    {
+        img: './image/openhci/0.png',
+        name: 'OpenHCI',
+        link: 'https://www.2022.openhci.com/',
+        badge: 'WEB',
+        tool: [
+            { tools: 'HTML' },
+            { tools: 'JS' },
+            { tools: 'CSS' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: 'OpenHCI是由學生自治籌備的人機互動工作坊，本活動希望能夠以推廣HCI為主要宗旨，招收工程、設計及跨領域背景的學生，共同學習並進行跨領域的合作，探索人類生活空間中的各種互動模式。此網站為第12屆OpenHCI工作坊之形象網站，內容包括展示講者、主辦者資訊與詳細日程、報名方式等等。',
+        Eintro: "OpenHCI is a student-organized workshop on human-computer interaction (HCI), aiming to promote HCI as its main mission. The workshop invites students from engineering, design, and interdisciplinary backgrounds to learn and collaborate across fields, exploring various interaction models within human living spaces. This website serves as the official site for the 12th OpenHCI workshop, providing information such as speaker and organizer profiles, detailed schedules, registration methods, and more.",
+        imgPosition: ''
+    },
+    {
+        img: './image/Routine/0.jpg',
+        name: 'Routine',
+        badge: 'GAME',
+        tool: [
+            { tools: 'UNITY' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '在這個遊戲中，你將扮演一位工廠品管作業員，藉由工作獲取報酬。 然而，工作並不是你生活的一切，每當下班時間來臨，你便可使用薪水探索不同的虛擬世界，享受片刻的放鬆。 但究竟要這樣日復一日地完成枯燥煩悶的工作並到虛擬世界逃避現實到什麼時候呢？',
+        Eintro: "In this game, you will play the role of a factory quality control worker, earning rewards through your job. However, work is not everything in your life. When it's time to clock out, you can use your salary to explore different virtual worlds and enjoy a moment of relaxation. But how long can you keep completing tedious and monotonous tasks day after day, only to escape reality into the virtual world?",
+        imgPosition: '75% center'
+    },
+
+];
