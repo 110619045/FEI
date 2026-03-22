0a366ef68e576f552006cba93c61c470e73165e6
110619045 <0304phoebe@gmail.com>
2026-03-23 05:55:17 +0800

    showcase

---
+++ js/showcase.js
@@ -0,0 +1,33 @@
+const WORKS_DATA = [
+    {
+        img: 'image/Illusory Eyescape/0.webp',
+        link: 'work/IllusoryEyescape.html',
+        Ename: 'Illusory Eyescape',
+        badge: 'DIGITAL ART',
+        tool: [{ tools: 'HTML' }, { tools: 'JS' }],
+        Esynopsis: 'In recent years, the rapid development of generative AI...',
+        imgPosition: 'center 33%'
+    },
+    {
+        img: 'image/PG/PG.png',
+        name: 'Pacific Graphics 2025',
+        link: 'work/PG.html',
+        Ename: 'Pacific Graphics 2025',
+        badge: 'CONFERENCE',
+        tool: [
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: ''
+    },
+    {
+        img: './image/友善地圖/01.png',
+        link: 'work/FriendlyMaps.html',
+        Ename: 'Friendly Maps',
+        badge: 'APP & MINI APP',
+        tool: [{ tools: 'React Native' }],
+        Esynopsis: 'Built on the Google Maps platform...',
+        imgPosition: 'center center'
+    }
+];
