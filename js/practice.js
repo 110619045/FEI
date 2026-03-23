af66354665162f62395cd34f017c18fcaa9782a1
110619045 <0304phoebe@gmail.com>
2026-03-23 09:56:00 +0800

    openhci

--- js/practice.js
+++ js/practice.js
@@ -37,22 +37,22 @@ const WORKS_DATA = [
         intro: '現今的社會型態使得越來越多人必須搬到非成長的都市打拼生活。離開家鄉長年孤單一人在外地，總會想帶一些充滿回憶的物品在新的住處陪伴自己。因此藉由選擇回憶中的物品作為一個契機，發想出一款近似人格測試的遊戲，檢視目前玩家所嚮往的未來與了解玩家努力生活的動力來源。',
         Eintro: "In today's society, more and more people are moving to cities different from where they grew up in order to pursue their careers. Living alone in a new place away from their hometown for years often makes them want to bring along items full of memories to keep them company. Inspired by this, we developed a game similar to a personality test that encourages players to choose items from their memories. This serves as an opportunity to examine the future they aspire to and understand the sources of motivation driving their efforts in life.",
     },
-    {
-        img: './image/openhci/0.png',
-        name: 'OpenHCI',
-        link: 'https://www.2022.openhci.com/',
-        badge: 'WEB',
-        tool: [
-            { tools: 'HTML' },
-            { tools: 'JS' },
-            { tools: 'CSS' },
-        ],
-        synopsis: '',
-        Esynopsis: '',
-        intro: 'OpenHCI是由學生自治籌備的人機互動工作坊，本活動希望能夠以推廣HCI為主要宗旨，招收工程、設計及跨領域背景的學生，共同學習並進行跨領域的合作，探索人類生活空間中的各種互動模式。此網站為第12屆OpenHCI工作坊之形象網站，內容包括展示講者、主辦者資訊與詳細日程、報名方式等等。',
-        Eintro: "OpenHCI is a student-organized workshop on human-computer interaction (HCI), aiming to promote HCI as its main mission. The workshop invites students from engineering, design, and interdisciplinary backgrounds to learn and collaborate across fields, exploring various interaction models within human living spaces. This website serves as the official site for the 12th OpenHCI workshop, providing information such as speaker and organizer profiles, detailed schedules, registration methods, and more.",
-        imgPosition: ''
-    },
+    // {
+    //     img: './image/openhci/0.png',
+    //     name: 'OpenHCI',
+    //     link: 'https://www.2022.openhci.com/',
+    //     badge: 'WEB',
+    //     tool: [
+    //         { tools: 'HTML' },
+    //         { tools: 'JS' },
+    //         { tools: 'CSS' },
+    //     ],
+    //     synopsis: '',
+    //     Esynopsis: '',
+    //     intro: 'OpenHCI是由學生自治籌備的人機互動工作坊，本活動希望能夠以推廣HCI為主要宗旨，招收工程、設計及跨領域背景的學生，共同學習並進行跨領域的合作，探索人類生活空間中的各種互動模式。此網站為第12屆OpenHCI工作坊之形象網站，內容包括展示講者、主辦者資訊與詳細日程、報名方式等等。',
+    //     Eintro: "OpenHCI is a student-organized workshop on human-computer interaction (HCI), aiming to promote HCI as its main mission. The workshop invites students from engineering, design, and interdisciplinary backgrounds to learn and collaborate across fields, exploring various interaction models within human living spaces. This website serves as the official site for the 12th OpenHCI workshop, providing information such as speaker and organizer profiles, detailed schedules, registration methods, and more.",
+    //     imgPosition: ''
+    // },
     {
         img: './image/Routine/0.jpg',
         name: 'Routine',
