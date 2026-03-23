9c8ccb6e22e1c1fe80ffd2686cb4bb2151530afb
110619045 <0304phoebe@gmail.com>
2026-03-23 08:21:59 +0800

    change class

---
+++ js/playground.js
@@ -0,0 +1,138 @@
+const WORKS_DATA = [
+    {
+        img: './image/play around with LoRA/0.webp',
+        Ename: 'play around with LoRA',
+        badge: 'CREATIVE',
+        tool: [
+            { tools: 'Python' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '嘗試使用LoRA訓練不同風格的模型。從網路上蒐集了20張漫畫風格肖像畫作品進行訓練。',
+        Eintro: "I tried using LoRA to train models in different styles. I collected 20 manga-style portrait artworks from the internet for training.",
+        layout: 'short', // tall / mid / short
+        imgPosition: ''
+    },
+    {
+        img: 'image/故事盒/box.webp',
+        name: '"?"',
+        Ename: '"?"',
+        badge: 'CREATIVE',
+        tool: [
+            { tools: 'AI' },
+            { tools: 'Electronics' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'tall',
+        imgPosition: ''
+    },
+    {
+        img: 'image/inkpaint/daydream.webp',
+        Ename: 'Daydream',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Ink' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'tall',
+        imgPosition: 'center 65%'
+    },
+    {
+        img: 'image/inkpaint/blessed.webp',
+        name: '為美好的世界獻上祝福',
+        Ename: 'May this beautiful world be blessed',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Ink' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'tall',
+        imgPosition: ''
+    },
+    {
+        img: 'image/inkpaint/moon.webp',
+        name: '迢迢新秋夕 亭亭月將圓',
+        Ename: 'The pale moon slowly rounds',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Ink' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'tall',
+        imgPosition: 'center 28%'
+    },
+    {
+        img: 'image/inkpaint/forgetting.webp',
+        name: '相望相忘',
+        Ename: 'A glance into forgetting',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Ink' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'tall',
+        imgPosition: 'center 58%'
+    },
+    {
+        img: 'image/watercolor/jiufen.webp',
+        name: '九份',
+        Ename: 'Jiufen',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Watercolor' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'mid',
+        imgPosition: ''
+    },
+    {
+        img: 'image/watercolor/tranquil.webp',
+        name: '靜',
+        Ename: 'Tranquil',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Watercolor' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'mid',
+        imgPosition: 'center 0%'
+    },
+
+    {
+        img: 'image/watercolor/181120_0026.webp',
+        name: '',
+        badge: 'PAINTING',
+        tool: [
+            { tools: 'Watercolor' },
+        ],
+        synopsis: '',
+        Esynopsis: '',
+        intro: '',
+        Eintro: "",
+        layout: 'mid',
+        // imgPosition: 'center 30%'
+    },
+
+
+];
